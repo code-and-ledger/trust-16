457a094d29bb46b6e79b025240fcf6754c53f0d3
Ahmed0-0Kallel <contactahmedkallel0@gmail.com>
2024-09-25 21:50:11 +0200

    Enhanced Landing Page with dynamic mouse effects and revamped Evervault card design; integrated Player Dashboard for interaction

--- core/client/src/components/landing-page.tsx
+++ core/client/src/components/landing-page.tsx
@@ -1,85 +1,132 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, CardContent } from "@/components/ui/card";
-import Image from 'next/image';
-import Main from './Main';  // Assuming Main component is in the same directory
+import Main from './main';  // Assuming Main component is in the same directory
 import SplashScreen from './splash-screen';
-import { EvervaultCard} from './ui/evervault-card';
+import { EvervaultCard } from './ui/evervault-card';
 import GameModeSelector from './game-mode-selector';
+import PlayerDashboard from './ui/answer-history';
+import { motion, useMotionTemplate, useMotionValue } from 'framer-motion';
+const CryptoBackground = () => {
+    let mouseX = useMotionValue(0);
+    let mouseY = useMotionValue(0);
 
-const GameModeCard: React.FC<{ mode: string }> = ({ mode }) => {
-    const [isHovered, setIsHovered] = useState(false);
+    const [randomString, setRandomString] = useState("");
+
+    useEffect(() => {
+        let str = generateRandomString(3000);
+        setRandomString(str);
+    }, []);
+
+    function onMouseMove({ currentTarget, clientX, clientY }: React.MouseEvent<HTMLDivElement>) {
+        let { left, top } = currentTarget.getBoundingClientRect();
+        mouseX.set(clientX - left);
+        mouseY.set(clientY - top);
+
+        const str = generateRandomString(3000);
+        setRandomString(str);
+    }
+
+    let maskImage = useMotionTemplate`radial-gradient(150px at ${mouseX}px ${mouseY}px, white, transparent)`;
+    let style = { maskImage, WebkitMaskImage: maskImage };
 
     return (
-        <Card
-            className={`flex-1 w-32 cursor-pointer bg-transparent shadow-transparent border-transparent transition-all duration-300 ease-in-out ${isHovered ? 'scale-110' : ''}`}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-        >
-            <CardContent className={`p-3 flex flex-col items-center justify-center transition-all duration-300 ease-in-out ${isHovered ? 'shadow-lg shadow-blue-200' : ''}`}>
-                <Image src="/trust16.svg" alt={mode} width={50} height={50} className={`transition-all duration-300 ease-in-out ${isHovered ? 'brightness-110' : ''}`} />
-                <p className={`mt-2 text-sm font-semibold transition-all duration-300 ease-in-out ${isHovered ? 'text-blue-600' : ''}`}>{mode}</p>
-            </CardContent>
-        </Card>
+        <div className="absolute inset-0 overflow-hidden" onMouseMove={onMouseMove}>
+            <div className="absolute inset-0 bg-black opacity-80"></div>
+            <motion.div
+                className="absolute inset-0 bg-gradient-to-r from-green-500 to-blue-700 opacity-0 group-hover:opacity-100 transition duration-500"
+                style={style}
+            />
+            <motion.div
+                className="absolute inset-0 opacity-0 mix-blend-overlay group-hover:opacity-100"
+                style={style}
+            >
+                <p className="absolute inset-x-0 text-xs h-full break-words whitespace-pre-wrap text-white font-mono font-bold transition duration-500">
+                    {randomString}
+                </p>
+            </motion.div>
+        </div>
     );
 };
+
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+const generateRandomString = (length: number) => {
+    let result = "";
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+};
+
+
 
 const LandingPage: React.FC = () => {
     return (
         <>{false && <SplashScreen />}
             {true && <Main width="80vw" height="70vh">
-
-                <div className="w-full h-full p-4 flex flex-col gap-4">
-                    {/* Rewards pool liquidity */}
-
-
-                    {/* Active players, Total games, Power-up section */}
-                    <div className="flex gap-4">
-                        <Card className="bg-white flex-1 ">
-                            <CardContent className="p-2 flex justify-between items-center">
-                                <p className="text-sm font-semibold">Rewards pool liquidity</p>
-                                <p className="text-lg font-bold">$$$$$$$$$$$</p>
-                            </CardContent>
-                        </Card>
-                        <Card className="flex-1">
-                            <CardContent className="p-3 flex flex-row justify-between items-center">
-                                <p className="text-sm font-semibold">Active players</p>
-                                <p className="text-lg font-bold">1,234</p>
-                            </CardContent>
-                        </Card>
-                        <Card className="flex-1 text-white bg-black">
-                            <CardContent className="p-3 flex flex-row justify-between items-center">
-                                <p className="text-sm font-semibold">Total games</p>
-                                <p className="text-lg font-bold">5,678</p>
-                            </CardContent>
-                        </Card>
-                        <Card className="flex-1 text-white bg-black border-red-500">
-                            <CardContent className="p-3 flex flex-row justify-between items-center">
-                                <p className="text-sm font-semibold">Power-up</p>
-                                <p className="text-lg font-bold">Available</p>
-                            </CardContent>
-                        </Card>
+                <div className="w-full h-full flex flex-col">
+                    
+                    {/* Top section with cards */}
+                    <div className="flex-none p-4">
+                        <div className="flex gap-4">
+                            <Card className="bg-transparent text-white border-white flex-1">
+                                <CardContent className="p-2 flex justify-between items-center">
+                                    <p className="text-sm font-semibold">Rewards pool liquidity</p>
+                                    <p className="text-lg font-bold">$$$$$$$$$$$</p>
+                                </CardContent>
+                            </Card>
+                            <Card className="flex-1">
+                                <CardContent className="p-3 flex flex-row justify-between items-center">
+                                    <p className="text-sm font-semibold">Active players</p>
+                                    <p className="text-lg font-bold">1,234</p>
+                                </CardContent>
+                            </Card>
+                            <Card className="flex-1 text-white bg-black">
+                                <CardContent className="p-3 flex flex-row justify-between items-center">
+                                    <p className="text-sm font-semibold">Total games</p>
+                                    <p className="text-lg font-bold">5,678</p>
+                                </CardContent>
+                            </Card>
+                            <Card className="flex-1 text-white bg-gradient-to-r from-black to-red-900 border-red-500">
+                                <CardContent className="p-3 flex flex-row justify-between items-center">
+                                    <p className="text-sm font-semibold">Power-up</p>
+                                    <p className="text-lg font-bold">Available</p>
+                                </CardContent>
+                            </Card>
+                        </div>
+                        
                     </div>
-                    <div className="flex-1 rounded-lg flex items-center justify-center">
-                            <GameModeSelector   />
+
+                    {/* Middle section with game mode selector */}
+                    <div className="flex-none h-1/4 flex items-center justify-center">
+                        <GameModeSelector />
                     </div>
-                    <div className="w-full flex justify-start">
-                        <div className="w-1/2 flex items-center justify-center">
-                            <div className="max-w-sm mx-auto relative h-[20rem] mb-10">
-                                <EvervaultCard
-                                    imageUrl="/chicken.jpg"
-                                    level={5}
-                                    money={10000}
-                                />
+
+                    {/* Bottom section with left and right sides */}
+                    <div className="flex-grow flex">
+                        {/* Left side with CryptoBackground and EvervaultCard */}
+                        <div className="w-1/2 relative group flex items-center justify-center">
+                            <CryptoBackground />
+                            <div className="relative z-10 p-4 flex flex-col items-center justify-center">
+                                <div className="max-w-sm mx-auto relative h-[20rem]">
+                                    <EvervaultCard
+                                        imageUrl="/chicken.jpg"
+                                        level={5}
+                                        money={10000}
+                                    />
+                                </div>
                             </div>
                         </div>
+
+                        {/* Right side with Answer History */}
+                        <div className="w-1/2 bg-white p-4 flex flex-col items-center justify-center">
+                            <PlayerDashboard answers={['trust', 'bail', 'bail', 'trust', 'bail', 'bail', 'trust', 'bail', 'bail']} />
+                        </div>
                     </div>
-
-
-
                 </div>
             </Main>}
         </>
     );
 };
+
 
 export default LandingPage;
--- core/client/src/components/ui/evervault-card.tsx
+++ core/client/src/components/ui/evervault-card.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { useMotionValue, useMotionTemplate, motion } from "framer-motion";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 
@@ -16,68 +15,28 @@ export const EvervaultCard = ({
   money: number;
   className?: string;
 }) => {
-  let mouseX = useMotionValue(0);
-  let mouseY = useMotionValue(0);
-
-  const [randomString, setRandomString] = useState("");
-
-  useEffect(() => {
-    let str = generateRandomString(1500);
-    setRandomString(str);
-  }, []);
-
-  function onMouseMove({ currentTarget, clientX, clientY }: React.MouseEvent<HTMLDivElement>) {
-    let { left, top } = currentTarget.getBoundingClientRect();
-    mouseX.set(clientX - left);
-    mouseY.set(clientY - top);
-
-    const str = generateRandomString(1500);
-    setRandomString(str);
-  }
-
   return (
-    <div
-      className={cn(
-        "p-0.5 bg-transparent aspect-square flex items-center justify-center w-full h-full relative",
-        className
-      )}
-    >
-      <div
-        onMouseMove={onMouseMove}
-        className="group/card rounded-3xl w-full relative overflow-hidden bg-transparent flex items-center justify-center h-full"
-      >
-        <CardPattern
-          mouseX={mouseX}
-          mouseY={mouseY}
-          randomString={randomString}
-        />
-        <div className="relative z-10 flex flex-col items-center justify-center">
-          <div className="relative h-44 w-44 rounded-full flex items-center justify-center overflow-hidden mb-4">
-            <div className="absolute w-full h-full bg-white/[0.8] dark:bg-black/[0.8] blur-sm rounded-full" />
-            <Image
-              src={imageUrl}
-              alt="Player"
-              width={176}
-              height={176}
-              className="rounded-full z-20"
-            />
+    <div className={cn("flex flex-col items-center justify-center", className)}>
+      <div className="w-36 h-36 mb-4">
+        <div className="w-full h-full rotate-45 overflow-hidden">
+          <Image
+            src={imageUrl}
+            alt="Player"
+            width={176}
+            height={176}
+            className="-rotate-45 scale-[1.5] origin-center"
+          />
+        </div>
+      </div>
+      <div className="flex space-x-20">
+        <div className="w-20 h-20 rotate-45 flex items-center justify-center border border-white bg-black/30">
+          <div className="-rotate-45 text-white font-bold text-lg">
+            Lvl {level}
           </div>
-          <div className="flex space-x-4">
-            <div className="relative h-20 w-20 rounded-full flex items-center justify-center overflow-hidden">
-              <div className="absolute inset-0 border-[1.5px] border-white rounded-full " />
-              <div className="absolute inset-0 bg-black/30 rounded-full" />
-              <div className="z-20 text-white font-bold text-lg">
-                Lvl {level}
-              </div>
-            </div>
-            <div className="relative h-20 w-20 rounded-full flex items-center justify-center overflow-hidden">
-              {/* <div className="absolute inset-0 border-4 border-white rounded-full blur-[10px]" /> */}
-              <div className="absolute inset-0 border-[1.5px] border-white rounded-full " />
-              <div className="absolute inset-0 bg-black/30 rounded-full" />
-              <div className="z-20 text-white font-bold text-lg">
-                ${money.toLocaleString()}
-              </div>
-            </div>
+        </div>
+        <div className="w-20 h-20 rotate-45 flex items-center justify-center border border-white bg-black/30">
+          <div className="-rotate-45 text-white font-bold text-lg">
+            ${money.toLocaleString()}
           </div>
         </div>
       </div>
@@ -85,37 +44,7 @@ export const EvervaultCard = ({
   );
 };
 
-export function CardPattern({ mouseX, mouseY, randomString }: any) {
-  let maskImage = useMotionTemplate`radial-gradient(250px at ${mouseX}px ${mouseY}px, white, transparent)`;
-  let style = { maskImage, WebkitMaskImage: maskImage };
 
-  return (
-    <div className="pointer-events-none">
-      <div className="absolute inset-0 rounded-2xl [mask-image:linear-gradient(white,transparent)] group-hover/card:opacity-50"></div>
-      <motion.div
-        className="absolute inset-0 rounded-2xl bg-gradient-to-r from-green-500 to-blue-700 opacity-0 group-hover/card:opacity-100 backdrop-blur-xl transition duration-500"
-        style={style}
-      />
-      <motion.div
-        className="absolute inset-0 rounded-2xl opacity-0 mix-blend-overlay group-hover/card:opacity-100"
-        style={style}
-      >
-        <p className="absolute inset-x-0 text-xs h-full break-words whitespace-pre-wrap text-white font-mono font-bold transition duration-500">
-          {randomString}
-        </p>
-      </motion.div>
-    </div>
-  );
-}
-
-const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-export const generateRandomString = (length: number) => {
-  let result = "";
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return result;
-};
 export const Icon = ({ className, ...rest }: any) => {
   return (
     <svg
