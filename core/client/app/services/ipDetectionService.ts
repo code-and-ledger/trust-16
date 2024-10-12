8b1c09dd437b99321a656edf54a5927bc4de1965
Aladeenb <alabtk@gmail.com>
2024-10-12 23:03:26 +0200

    Restructured repo

--- core/client/app/services/ipDetectionService.ts
+++
@@ -1,5 +0,0 @@
-import { playersQueue } from "./matchMakingService";
-
-export const detectSameIP = (ip: string): boolean => {
-  return playersQueue.some(player => player.ip === ip);
-};
--- core/client/app/utils/encryption/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './generatePepper';
-export * from './hashWithPepper';
-export * from './pepperStore';
