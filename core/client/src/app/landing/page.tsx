3de1d7fbb61aa15e548103492de15a92aa41390a
Ahmed0-0Kallel <contactahmedkallel0@gmail.com>
2024-10-13 15:06:04 +0200

    Fixed layout, updated short game mode

--- core/client/src/app/landing/page.tsx
+++ core/client/src/app/landing/page.tsx
@@ -1,83 +1,76 @@
-'use client';
-import React, { useEffect, useState } from 'react';
+"use client";
+import React, { useEffect, useState } from "react";
 import { Card, CardContent } from "../../components/ui/card";
-import Main from "../../components/main";  // Assuming Main component is in the same directory
+import Main from "../../components/main"; // Assuming Main component is in the same directory
 import SplashScreen from "../../components/splash-screen";
 import GameModeSelector from "../../components/game-mode-selector";
 import PlayerDashboard from "../../components/ui/answer-history";
-import { EvervaultCard } from '../../components/ui/evervault-card';
-
-
-
+import { EvervaultCard } from "../../components/ui/evervault-card";
 
 const LandingPage: React.FC = () => {
     return (
-        <>{false && <SplashScreen />}
-            {true && <Main width="80vw" height="70vh">
-                <div className="w-full h-full flex flex-col">
-                    
-                    {/* Top section with cards */}
-                    <div className="flex-none p-4">
-                        <div className="flex gap-4">
-                            <Card className="bg-transparent text-white border-white flex-1">
-                                <CardContent className="p-2 flex justify-between items-center">
-                                    <p className="text-sm font-semibold">Rewards pool liquidity</p>
-                                    <p className="text-lg font-bold">$$$$$$$$$$$</p>
-                                </CardContent>
-                            </Card>
-                            <Card className="flex-1">
-                                <CardContent className="p-3 flex flex-row justify-between items-center">
-                                    <p className="text-sm font-semibold">Active players</p>
-                                    <p className="text-lg font-bold">1,234</p>
-                                </CardContent>
-                            </Card>
-                            <Card className="flex-1 text-white bg-black">
-                                <CardContent className="p-3 flex flex-row justify-between items-center">
-                                    <p className="text-sm font-semibold">Total games</p>
-                                    <p className="text-lg font-bold">5,678</p>
-                                </CardContent>
-                            </Card>
-                            <Card className="flex-1 text-white bg-gradient-to-r from-black to-red-900 border-red-500">
-                                <CardContent className="p-3 flex flex-row justify-between items-center">
-                                    <p className="text-sm font-semibold">Power-up</p>
-                                    <p className="text-lg font-bold">Available</p>
-                                </CardContent>
-                            </Card>
-                        </div>
-                        
-                    </div>
+        <>
+            {false && <SplashScreen />}
+            <div className="bg-background w-screen h-screen overflow-hidden relative">
+                <div className={`absolute top-0 bottom-0 right-1/2 bg-black transition-all duration-500 ease-in-out w-1/2 h-full`} />
 
-                    {/* Middle section with game mode selector */}
-                    <div className="flex-none h-1/4 flex items-center justify-center">
-                        <GameModeSelector />
-                    </div>
+                {true && (
+                    <Main width="80vw" height="70vh">
+                        <div className="w-full h-full p-4 flex flex-col gap-4">
+                            {/* Active players, Total games, Power-up section */}
+                            <div className="flex gap-4">
+                                <Card className="bg-white flex-1 ">
+                                    <CardContent className="p-2 flex justify-between items-center">
+                                        <p className="text-sm font-semibold">Rewards pool liquidity</p>
+                                        <p className="text-lg font-bold">$$$$$$$$$$$</p>
+                                    </CardContent>
+                                </Card>
+                                <Card className="flex-1">
+                                    <CardContent className="p-3 flex flex-row justify-between items-center">
+                                        <p className="text-sm font-semibold">Active players</p>
+                                        <p className="text-lg font-bold">1,234</p>
+                                    </CardContent>
+                                </Card>
+                                <Card className="flex-1 text-white bg-black">
+                                    <CardContent className="p-3 flex flex-row justify-between items-center">
+                                        <p className="text-sm font-semibold">Total games</p>
+                                        <p className="text-lg font-bold">5,678</p>
+                                    </CardContent>
+                                </Card>
+                                <Card className="flex-1 text-white bg-black border-red-500">
+                                    <CardContent className="p-3 flex flex-row justify-between items-center">
+                                        <p className="text-sm font-semibold">Power-up</p>
+                                        <p className="text-lg font-bold">Available</p>
+                                    </CardContent>
+                                </Card>
+                            </div>
+                            {/* Middle section with game mode selector */}
+                            <div className="flex-1 rounded-lg flex items-center justify-center">
+                                <GameModeSelector />
+                            </div>
 
-                    {/* Bottom section with left and right sides */}
-                    <div className="flex-grow flex">
-                        {/* Left side with CryptoBackground and EvervaultCard */}
-                        <div className="w-1/2 bg-black relative group flex items-center justify-center">
-                       
-                            <div className="relative z-10 p-4 flex flex-col items-center justify-center">
-                                <div className="max-w-sm mx-auto relative h-[20rem]">
-                                    <EvervaultCard
-                                        imageUrl="/chicken.jpg"
-                                        level={5}
-                                        money={10000}
-                                    />
+                            {/* Bottom section with left and right sides */}
+                            <div className="flex-grow flex">
+                                {/* Left side with CryptoBackground and EvervaultCard */}
+                                <div className="w-1/2  relative group flex items-center justify-center">
+                                    <div className="relative z-10 p-4 flex flex-col items-center justify-center">
+                                        <div className="max-w-sm mx-auto relative h-[20rem]">
+                                            <EvervaultCard imageUrl="/chicken.jpg" level={5} money={10000} />
+                                        </div>
+                                    </div>
+                                </div>
+
+                                {/* Right side with Answer History */}
+                                <div className="w-1/2  p-4 flex flex-col items-center justify-center">
+                                    <PlayerDashboard answers={["trust", "bail", "bail", "trust", "bail", "bail", "trust", "bail", "bail"]} />
                                 </div>
                             </div>
                         </div>
-
-                        {/* Right side with Answer History */}
-                        <div className="w-1/2 bg-white p-4 flex flex-col items-center justify-center">
-                            <PlayerDashboard answers={['trust', 'bail', 'bail', 'trust', 'bail', 'bail', 'trust', 'bail', 'bail']} />
-                        </div>
-                    </div>
-                </div>
-            </Main>}
+                    </Main>
+                )}
+            </div>
         </>
     );
 };
-
 
 export default LandingPage;
--- core/client/src/app/short-game/page.tsx
+++ core/client/src/app/short-game/page.tsx
@@ -1,76 +1,229 @@
-import { Settings, Diamond } from 'lucide-react';
-import Image from 'next/image';
+"use client"
+import { useState, useEffect } from 'react'
+import { Settings, Diamond, Info } from 'lucide-react'
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import Main from "../../components/main"
+import { EvervaultCard } from "../../components/ui/evervault-card"
 
-export default function ShortGameMode() {
-  return (
-    <div className="bg-black text-white p-4 w-screen h-screen flex justify-center items-center font-mono">
-      <div className="border border-white p-4 rounded max-w-full w-full h-full flex flex-col justify-center items-center">
-        
-        {/* Game Mode Title */}
-        <div className="text-center mb-6 border-b border-white pb-2 text-lg md:text-4xl w-full">
-          Short Game mode
+
+// GameInfoDialog component
+const GameInfoDialog = () => (
+  <Dialog>
+    <DialogTrigger asChild>
+      <Button variant="ghost" size="sm" className="ml-2">
+        <Info className="w-5 h-5" />
+      </Button>
+    </DialogTrigger>
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Short Game Mode Rules</DialogTitle>
+      </DialogHeader>
+      <Tabs defaultValue="setup">
+        <TabsList>
+          <TabsTrigger value="setup">Setup</TabsTrigger>
+          <TabsTrigger value="rounds">Rounds</TabsTrigger>
+          <TabsTrigger value="distribution">Distribution</TabsTrigger>
+        </TabsList>
+        <TabsContent value="setup">
+          <ul className="list-disc pl-5">
+            <li>Each player deposits dp TRUST</li>
+            <li>The rewards pool contributes cr TRUST</li>
+            <li>Total Game Pool starts at 2dp + cr TRUST</li>
+          </ul>
+        </TabsContent>
+        <TabsContent value="rounds">
+          <p>The game consists of 5 rounds. In each round:</p>
+          <ul className="list-disc pl-5">
+            <li>Players simultaneously choose to either Cooperate (Green) or Compete (Red)</li>
+            <li>Choices are revealed, and TRUST is redistributed based on the decisions</li>
+          </ul>
+        </TabsContent>
+        <TabsContent value="distribution">
+          <table className="w-full border-collapse">
+            <thead>
+              <tr className="bg-gray-200">
+                <th className="border p-2">Scenario</th>
+                <th className="border p-2">Player 1</th>
+                <th className="border p-2">Player 2</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="border p-2">Green-Green</td>
+                <td className="border p-2">rc from deposit + rc from rewards pool</td>
+                <td className="border p-2">rc from deposit + rc from rewards pool</td>
+              </tr>
+              <tr>
+                <td className="border p-2">Red-Green</td>
+                <td className="border p-2">Previous balance + Player 2's balance + rt from Player 2</td>
+                <td className="border p-2">rc from rewards pool</td>
+              </tr>
+              <tr>
+                <td className="border p-2">Red-Red</td>
+                <td className="border p-2">0 TRUST</td>
+                <td className="border p-2">0 TRUST</td>
+              </tr>
+            </tbody>
+          </table>
+        </TabsContent>
+      </Tabs>
+    </DialogContent>
+  </Dialog>
+)
+
+// PlayerCard component
+const PlayerCard = ({ username, balance, isOpponent = false }) => (
+  <div className={`text-center`}>
+    <div className="max-w-sm mx-auto relative h-[20rem]">
+      <EvervaultCard imageUrl="/flinch.jpg" level={5} money={balance} />
+    </div>
+    <div className="text-xl mt-4">{username}</div>
+    <div className="tracking-wider text-sm">
+      {balance !== undefined ? `${balance} TRUST` : '◇◇◇◇◇◇◇◇◇◇'}
+    </div>
+  </div>
+)
+
+// ChoiceButtons component
+const ChoiceButtons = ({ playerChoice, setPlayerChoice, disabled }) => (
+  <div className="flex justify-center space-x-8">
+    <Button
+      variant="outline"
+      size="lg"
+      className={`text-2xl text-primary font-bold transition-all duration-300 ${playerChoice === 'green' ? 'bg-green-500 text-white scale-110' : 'hover:bg-green-200'}`}
+      onClick={() => setPlayerChoice('green')}
+      disabled={disabled}
+    >
+      Cooperate
+    </Button>
+    <Button
+      variant="outline"
+      size="lg"
+      className={` text-primary text-2xl font-bold transition-all duration-300 ${playerChoice === 'red' ? 'bg-red-500 text-white scale-110' : 'hover:bg-red-200'}`}
+      onClick={() => setPlayerChoice('red')}
+      disabled={disabled}
+    >
+      Compete
+    </Button>
+  </div>
+)
+
+// RoundResult component
+const RoundResult = ({ playerChoice, opponentChoice }) => (
+  <div className="flex justify-center items-center mb-8">
+    <div className="flex rounded-full overflow-hidden">
+      <div className="bg-white border-2 border-black p-4">
+        <div className="text-3xl font-bold flex items-center">
+          <span className="text-black">
+            {playerChoice === 'green' ? 'G' : (playerChoice === 'red' ? 'R' : '-')}
+          </span>
         </div>
-
-        {/* Announcement Section */}
-        <div className="text-xs md:text-sm mb-6 text-center w-full">
-          announcements. custom-messages. announcements.
-        </div>
-
-        {/* Rewards Pool Section */}
-        <div className="text-center mb-6 text-base md:text-xl w-full">
-          Rewards pool liquidity $$$
-        </div>
-
-        {/* Player Information Section */}
-        <div className="flex justify-between items-center mb-6 w-full px-10">
-          {/* Player 1 */}
-          <div className="text-center">
-            <div className="mb-2 text-sm md:text-lg">$username</div>
-            <div className="tracking-wider text-xs md:text-base">◇◇◇◇◇◇◇◇◇◇</div>
-          </div>
-          
-          {/* Round and Score */}
-          <div className="text-center">
-            <div className="border border-white p-2 rounded">
-                <span className="text-sm md:text-lg">4</span>
-              <Diamond className="inline-block mr-2 ml-2 w-4 h-4 md:w-6 md:h-6" />
-              <span className="text-sm md:text-lg">4</span>
-            </div>
-            <div className="mt-2 text-sm md:text-lg">ROUND 6</div>
-          </div>
-          
-          {/* Player 2 */}
-          <div className="text-center">
-            <div className="mb-2 text-sm md:text-lg">$username</div>
-            <div className="tracking-wider text-xs md:text-base">◇◇◇◇◇◇◇◇◇◇</div>
-          </div>
-        </div>
-
-        {/* Player Images */}
-        <div className="flex justify-around mb-6 w-full px-10">
-          <div>
-            <Image src="/flinch.jpg" alt="Player 1" width={75} height={75} className="rounded-full" />
-          </div>
-          <div>
-            <Image src="/flinch.jpg" alt="Player 2" width={75} height={75} className="transform scale-x-[-1] rounded-full" />
-          </div>
-        </div>
-
-        {/* Buttons */}
-        <div className="flex justify-between w-full px-10">
-          <button className="border border-white px-6 py-2 rounded flex items-center text-sm md:text-base">
-            <Settings className="w-5 h-5 mr-2 md:w-6 md:h-6" />
-            settings
-          </button>
-          <button className="border border-white px-6 py-2 rounded text-sm md:text-base">
-            $trust
-          </button>
-          <button className="border border-white px-6 py-2 rounded flex items-center text-sm md:text-base">
-            short
-            <Diamond className="w-5 h-5 ml-2 md:w-6 md:h-6" />
-          </button>
+      </div>
+      <div className="bg-black border-2 border-white p-4">
+        <div className="text-3xl font-bold flex items-center">
+          <span className="text-white">
+            {opponentChoice === 'green' ? 'G' : (opponentChoice === 'red' ? 'R' : '-')}
+          </span>
         </div>
       </div>
     </div>
-  );
+  </div>
+)
+
+export default function ShortGameMode() {
+  const [round, setRound] = useState(1)
+  const [playerChoice, setPlayerChoice] = useState<'green' | 'red' | null>(null)
+  const [opponentChoice, setOpponentChoice] = useState<'green' | 'red' | null>(null)
+  const [gamePool, setGamePool] = useState(100)
+  const [playerBalance, setPlayerBalance] = useState(50)
+
+  useEffect(() => {
+    if (playerChoice) {
+      setTimeout(() => {
+        setOpponentChoice(Math.random() > 0.5 ? 'green' : 'red')
+      }, 1000)
+    }
+  }, [playerChoice])
+
+
+  // TODO: This is automatic should be done when the time of the round ends
+  const advanceRound = () => {
+    if (playerChoice === 'green' && opponentChoice === 'green') {
+      setPlayerBalance(prev => prev + 10)
+      setGamePool(prev => prev - 20)
+    } else if (playerChoice === 'red' && opponentChoice === 'green') {
+      setPlayerBalance(prev => prev + 20)
+      setGamePool(prev => prev - 20)
+    }
+
+    if (round < 5) setRound(round + 1)
+    setPlayerChoice(null)
+    setOpponentChoice(null)
+  }
+
+  return (
+    <div className="bg-background w-screen h-screen overflow-hidden relative">
+      <div className="absolute top-0 bottom-0 right-1/2 bg-black transition-all duration-500 ease-in-out w-1/2 h-full" />
+      <Main width="90vw" height="90vh">
+        <div className="w-full h-full p-4 flex font-mono flex-col">
+          <div className="w-full flex flex-row">
+            {/* Left half (black) */}
+            <div className="text-white w-1/2">
+              <div className="text-3xl mb-4">Round {round} of 5</div>
+              <div className="text-xl mb-6">
+                Total Game Pool: {gamePool} TRUST
+              </div>
+            </div>
+            {/* Right half (white) */}
+            <div className="text-black w-1/2">
+              <div className="flex text-black justify-end">
+                <GameInfoDialog />
+              </div>
+            </div>
+          </div>
+          
+          <div className="flex flex-row justify-center">
+            <RoundResult playerChoice={playerChoice} opponentChoice={opponentChoice} />
+          </div>
+          
+          <div className="w-full flex flex-row">
+            {/* Left half (black) */}
+            <div className="w-1/2 text-white p-8 flex flex-col">
+              <div className="flex-1 flex flex-col justify-between">
+                <PlayerCard username="$username" balance={playerBalance} />
+                
+                <ChoiceButtons
+                  playerChoice={playerChoice}
+                  setPlayerChoice={setPlayerChoice}
+                  disabled={!!opponentChoice}
+                />
+              </div>
+            </div>
+
+            {/* Right half (white) */}
+            <div className="w-1/2 text-black p-8 flex flex-col">
+              <div className="flex-1 flex flex-col justify-between">
+                <PlayerCard username="$opponent" balance={gamePool - playerBalance} isOpponent />
+
+                <div className="flex justify-center mt-8">
+                  <Button
+                    variant="default"
+                    size="lg"
+                    className="px-8 py-4 text-xl"
+                    onClick={advanceRound}
+                    disabled={!playerChoice || !opponentChoice}
+                  >
+                    Next Round
+                  </Button> 
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Main>
+    </div>
+  )
 }
